864a72f61d241b478c63993cc2e8402d2f54b854
Alex Lawrence <alexjlawrence88@gmail.com>
2020-05-20 22:10:35 +0100

    techpulse email

--- helpers/emailHelper.js
+++ helpers/emailHelper.js
@@ -30,5 +30,25 @@ module.exports = () => {
       });
     });
   };
+
+  this.sendEmailTechPulse = (addr, msg, subject) => {
+    return new Promise((resolve, reject) => {
+      const mailOptions = {
+        from: process.env.EMAIL_FROM, // sender address
+        to: addr, // list of receivers
+        subject: subject, // Subject line
+        html: msg // plain text body
+      };
+
+      transporter.sendMail(mailOptions, function(err, info) {
+        if (err) {
+          console.log(err);
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      });
+    });
+  };
   return this;
 };
--- routes/email.js
+++ routes/email.js
@@ -1,15 +1,28 @@
-const router = require('express').Router();
-const emailHelper = require('../helpers/emailHelper')();
+const router = require("express").Router();
+const emailHelper = require("../helpers/emailHelper")();
 
-router.get('/send', async (req, res) => {
-    const to = req.query.to;
-    const subject = req.query.subject;
-    const text = req.query.text;
+router.get("/send", async (req, res) => {
+  const to = req.query.to;
+  const subject = req.query.subject;
+  const text = req.query.text;
 
-    const result = await emailHelper.sendEmail(to, text, subject);
+  const result = await emailHelper.sendEmail(to, text, subject);
 
-    res.status(result ? 200 : 500).send(result ? 'Email sent.' : 'An error occured sending email');
+  res
+    .status(result ? 200 : 500)
+    .send(result ? "Email sent." : "An error occured sending email");
+});
 
-})
+router.get("/techpulse/send", async (req, res) => {
+  const to = "alexjlawrence88@gmail.com";
+  const subject = "techpulse enquiry";
+  const text = `Enquiry from ${req.query.email}: ${req.query.message}`;
 
-module.exports = router
+  const result = await emailHelper.sendEmailTechPulse(to, text, subject);
+
+  res
+    .status(result ? 200 : 500)
+    .send(result ? "Email sent." : "An error occured sending email");
+});
+
+module.exports = router;
